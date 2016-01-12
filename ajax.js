90893ea8933046e198c66f632a1de5c8dcfdefc8
Vera Zabeida <vera.zabeida@gmail.com>
2016-01-12 09:44:06 -0500

    testing api

--- ajax.js
+++ ajax.js
@@ -1,28 +1,9 @@
-// var API = require('./API_response');
-// console.log(API);
+var API = require('./API_response');
 
-function createCORSRequest(method, url) {
-  var xhr = new XMLHttpRequest();
-  if ("withCredentials" in xhr) {    
-    xhr.open(method, url, true);
-  } else if (typeof XDomainRequest != "undefined") {
-    xhr = new XDomainRequest();
-    xhr.open(method, url);
-  } else {
-    xhr = null;
+window.hello = function (){
+  for(var x = 0; x<API.items.length; x++){
+    console.log(API.items[x].url);  
   }
-  return xhr;
-}
-
-function search(city){
-  var city = city;
-  var url="https://api.github.com/search/users?q=location:"+city;
-  var xhr = createCORSRequest('GET', url);
-  xhr.onload = function(){
-    var text = xhr.responseText;
-    console.log(text);
-  }
-  xhr.send();
 }
 
 
--- bundle.js
+++ bundle.js
@@ -607,30 +607,11 @@ score: 1
 }
 },{}],2:[function(require,module,exports){
 var API = require('./API_response');
-console.log(API);
 
-function createCORSRequest(method, url) {
-  var xhr = new XMLHttpRequest();
-  if ("withCredentials" in xhr) {    
-    xhr.open(method, url, true);
-  } else if (typeof XDomainRequest != "undefined") {
-    xhr = new XDomainRequest();
-    xhr.open(method, url);
-  } else {
-    xhr = null;
+window.hello = function (){
+  for(var x = 0; x<API.items.length; x++){
+    console.log(API.items[x].url);  
   }
-  return xhr;
-}
-
-function search(city){
-  var city = city;
-  var url="https://api.github.com/search/users?q=location:"+city;
-  var xhr = createCORSRequest('GET', url);
-  xhr.onload = function(){
-    var text = xhr.responseText;
-    console.log(text);
-  }
-  xhr.send();
 }
 
 
