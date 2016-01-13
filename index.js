a02e51e516504c21509ff7a533acba6256d98599
Vera Zabeida <vera.zabeida@gmail.com>
2016-01-13 16:40:59 -0500

    react/redux env setup

---
+++ index.js
@@ -0,0 +1,27 @@
+var Redux = require('redux');
+var ReactDOM = require('react-dom');
+var React = require('react');
+
+//actions
+
+//reducers
+
+//store
+
+//components
+
+var Application = React.createClass({
+	render: function(){
+		return(<div>hi</div>);
+	}
+});
+
+ReactDOM.render(
+	<Application/>,
+	document.getElementById('app')
+);
+
+
+
+
+
